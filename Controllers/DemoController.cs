4173df8e96fcd9b0da26e29be542f9829bc56509
Julian Maldonado <jmalapo91@gmail.com>
2021-02-26 15:02:07 -0500

    Initial commit

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Swagger.Controllers
+{
+    [SwaggerTag("Demo", "www.prueba.com" )]
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class DemoController : ControllerBase
+    {
+        /// <summary>
+        /// Get string.
+        /// </summary>
+        /// <response code="401">Unauthorized. No se encuentra autorizado</response>
+        /// <response code="200">Ok. Correcto</response>
+        /// <response code="404">NotFound. No se encuentra</response>
+        /// <response code="500">Error. Se produjo un error</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(void), 500)]
+        public IActionResult GetString()
+        {
+            return Ok("Hi Word!");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+
+namespace Swagger
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Contact = new OpenApiContact
+                    {
+                        Email = "tuttek@gmail.com",
+                        Name = "TUTTEK",
+                        Url = new Uri("http://www.google.com")
+                    },
+                    Description = "Tutorial implementación API",
+                    Title = "TUTTEK SWAGGER",
+                    Version = "V1"
+                });
+
+                var filePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Swagger.xml");
+                swagger.IncludeXmlComments(filePath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tutorial Swagger V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
